d23d09dcf4d5353db4ed5c0cb4acaca4cc317783
marsislav <mkostadinov87@gmail.com>
2024-04-23 23:48:45 +0300

    Homepage fixes. Cleaning.

--- home-page.php
+++ home-page.php
@@ -5,7 +5,67 @@
  **/
 get_header(); ?>
 
-
+<!-- home page slider -->
+<div class="homepage-slider">
+		<!-- single home slider -->
+		<div class="single-homepage-slider homepage-bg-1">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12 col-lg-7 offset-lg-1 offset-xl-0">
+						<div class="hero-text">
+							<div class="hero-text-tablecell">
+								<p class="subtitle">Fresh & Organic</p>
+								<h1>Delicious Seasonal Fruits</h1>
+								<div class="hero-btns">
+									<a href="shop.html" class="boxed-btn">Fruit Collection</a>
+									<a href="contact.html" class="bordered-btn">Contact Us</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- single home slider -->
+		<div class="single-homepage-slider homepage-bg-2">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-10 offset-lg-1 text-center">
+						<div class="hero-text">
+							<div class="hero-text-tablecell">
+								<p class="subtitle">Fresh Everyday</p>
+								<h1>100% Organic Collection</h1>
+								<div class="hero-btns">
+									<a href="shop.html" class="boxed-btn">Visit Shop</a>
+									<a href="contact.html" class="bordered-btn">Contact Us</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- single home slider -->
+		<div class="single-homepage-slider homepage-bg-3">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-10 offset-lg-1 text-right">
+						<div class="hero-text">
+							<div class="hero-text-tablecell">
+								<p class="subtitle">Mega Sale Going On!</p>
+								<h1>Get December Discount</h1>
+								<div class="hero-btns">
+									<a href="shop.html" class="boxed-btn">Visit Shop</a>
+									<a href="contact.html" class="bordered-btn">Contact Us</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- end home page slider -->
 	<!-- features list section -->
 	<div class="list-section pt-80 pb-80">
 		<div class="container">
@@ -252,7 +312,7 @@ wp_reset_postdata();
         </div>
         <div class="row">
             <div class="col-lg-12 text-center">
-                <a href="<?php echo esc_url( get_permalink( get_option( 'page_for_posts' ) ) ); ?>" class="boxed-btn">More News</a>
+                <a href="<?php echo esc_url( get_permalink( get_option( 'page_for_posts' ) ) ); ?>" class="boxed-btn"><?php _e('View More', 'mexclusive21');?></a>
             </div>
         </div>
     </div>
