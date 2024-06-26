6c2bdd661c2014393d3d6c8b010a5692b721b49c
marsislav <mkostadinov87@gmail.com>
2024-06-26 08:05:37 +0300

    Navigation changes

--- functions/class-wp-bootstrap-navwalker.php
+++ functions/class-wp-bootstrap-navwalker.php
@@ -1,21 +1,5 @@
 <?php
-/**
- * WP Bootstrap Navwalker
- *
- * @package WP-Bootstrap-Navwalker
- *
- * @wordpress-plugin
- * Plugin Name: WP Bootstrap Navwalker
- * Plugin URI:  https://github.com/wp-bootstrap/wp-bootstrap-navwalker
- * Description: A custom WordPress nav walker class to implement the Bootstrap 4 navigation style in a custom theme using the WordPress built in menu manager.
- * Author: Edward McIntyre - @twittem, WP Bootstrap, William Patton - @pattonwebz, IanDelMar - @IanDelMar
- * Version: 4.3.0
- * Author URI: https://github.com/wp-bootstrap
- * GitHub Plugin URI: https://github.com/wp-bootstrap/wp-bootstrap-navwalker
- * GitHub Branch: master
- * License: GPL-3.0+
- * License URI: http://www.gnu.org/licenses/gpl-3.0.txt
- */
+
 
 // Check if Class Exists.
 if ( ! class_exists( 'WP_Bootstrap_Navwalker' ) ) :
